854e66e2e8e530819181cc7b44975e85237944c0
tnphammy <tnpham@smith.edu>
2025-02-23 20:42:19 -0500

    Added  boardCar()

--- Passenger.java
+++ Passenger.java
@@ -9,4 +9,18 @@ public class Passenger {
     public String toString() {
         return this.name;
     }
+
+    /**
+     * Boards a passenger to a car.
+     * @param c The car that the passenger wants to board
+     */
+    public void boardCar(Car c) {
+        if (c.addPassenger(this) == true) {
+            System.out.println(this.name + " has successfully boarded the car!");
+        }
+        else {
+            System.out.println(this.name + " could NOT board the car!");
+        }
+    }
+
 }
